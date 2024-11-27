b2972b9aeeacb0c22352e4cdac3a2c1c17e6ee63
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-11-28 00:52:06 +0530

    s added glyphs

---
+++ src/dashboard.rs
@@ -0,0 +1,3 @@
+pub struct Dashboard {
+    score: i32,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate piston_window;
 extern crate rand;
 
+mod dashboard;
 mod draw;
 mod game;
 mod snake;
+mod write;
 
 use piston_window::types::Color;
 use piston_window::*;
@@ -14,13 +16,14 @@ use game::Game;
 const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
 
 fn main() {
-    let (width, height) = (30, 30);
+    let (width, height) = (30, 40);
     let mut window: PistonWindow =
         WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
             .exit_on_esc(true)
             .build()
             .unwrap();
-    let mut game = Game::new(width, height);
+    let glyphs = write::load_fonts(&mut window);
+    let mut game = Game::new(width, 10);
     while let Some(event) = window.next() {
         if let Some(Button::Keyboard(key)) = event.press_args() {
             game.key_pressed(key);
@@ -29,9 +32,6 @@ fn main() {
             clear(BACK_COLOR, g);
             game.draw(&c, g);
         });
-        // event.update(|arg| {
-        //     game.update(arg.dt);
-        // })
         if let Some(update_args) = event.update_args() {
             game.update(update_args.dt);
         }
